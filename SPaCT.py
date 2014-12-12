017774bd3ace5889629c3727ca771702a9e673a2
zpace <zpace21@gmail.com>
2014-12-12 14:33:46 -0600

    initialized SparsePak Correction Tools (SPaCT)

---
+++ SPaCT.py
@@ -0,0 +1,12 @@
+'''
+SparsePak Correction Tools (SPaCT)
+Author: Zach Pace (U Wisc-Madison)
+
+Some tools for correcting un-flux-calibrated SparsePak data.
+
+Performance not guaranteed
+
+Changelog:
+Version 0.1 (Nov 2014) - correct using interpolated fiberflat
+Version 0.2 (Dec 2014) - add correction with SDSS spectrum
+'''
